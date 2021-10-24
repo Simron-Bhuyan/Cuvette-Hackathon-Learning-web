9b30ce3b40a8d289692819721f290d8fab31a181
SIMRON BHUYAN <simron0707@gmail.com>
2021-10-24 20:23:20 +0530

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,49 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {LoginComponent} from "./components/login/login.component";
+import {RegisterComponent} from "./components/register/register.component";
+import {CartComponent} from "./components/cart/cart.component";
+import {CheckoutComponent} from "./components/checkout/checkout.component";
+import {ProductComponent} from "./components/product/product.component";
+import {ThankyouComponent} from "./components/thankyou/thankyou.component";
+import { FooterComponent } from './components/footer/footer.component';
+
+
+const routes: Routes = [
+  {
+    path: '', redirectTo: '/login',pathMatch:'full'
+  },
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'register', component: RegisterComponent
+  },
+  {
+    path: 'product', component: ProductComponent
+  },
+  {
+    path: 'cart', component: CartComponent
+  },
+  {
+    path: 'checkout', component: CheckoutComponent
+  },
+  {
+    path: 'thankyou', component: ThankyouComponent
+  },
+  {
+    path: 'footer', component: FooterComponent
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
