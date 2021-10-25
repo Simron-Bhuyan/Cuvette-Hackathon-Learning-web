82944f124a4443024b4c91345ab05d7d963d4a9b
SIMRON BHUYAN <simron0707@gmail.com>
2021-10-25 11:58:48 +0530

    Update register.component.ts

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,12 +1,49 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { UserModel } from '../../models/user.model';
 
 @Component({
-  selector: "mg-register",
-  templateUrl: "./register.component.html",
-  styleUrls: ["./register.component.scss"],
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  // styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  constructor() {}
+roleUser: UserModel;
+  registerUserData = {
+    name: "",
+    contact: "",
+    email: "",
+    password: ""
+  };
+  // email: String;
+  // password: String;
 
-  ngOnInit() {}
+
+  constructor(private authService: AuthService, private router: Router) {
+
+
+   }
+
+
+  ngOnInit(): void {
+  }
+
+
+
+  registerUser(){
+
+    this.authService.registerUser(this.registerUserData).subscribe(
+      res => {
+        console.log(res);
+
+        localStorage.setItem('token', res.token);
+
+        this.router.navigate(['/login'])
+    },
+      err => console.log(err)
+    )
+
+  }
+
 }
