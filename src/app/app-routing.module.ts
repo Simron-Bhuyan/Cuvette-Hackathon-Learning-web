9e5fc0f0606d939459e82525bd75c62843bd3ac4
SIMRON BHUYAN <simron0707@gmail.com>
2021-10-25 11:44:19 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,60 +1,68 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { HomeComponent } from "./components/home/home.component";
-import { LoginComponent } from "./components/login/login.component";
-import { RegisterComponent } from "./components/register/register.component";
-import { CartComponent } from "./components/cart/cart.component";
-import { CheckoutComponent } from "./components/checkout/checkout.component";
-import { ProductComponent } from "./components/product/product.component";
-import { ThankyouComponent } from "./components/thankyou/thankyou.component";
-import { FooterComponent } from "./components/footer/footer.component";
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {LoginComponent} from "./components/login/login.component";
+import {RegisterComponent} from "./components/register/register.component";
+import {CartComponent} from "./components/cart/cart.component";
+import {CheckoutComponent} from "./components/checkout/checkout.component";
+import {ProductComponent} from "./components/product/product.component";
+import {ThankyouComponent} from "./components/thankyou/thankyou.component";
+import { FooterComponent } from './components/footer/footer.component';
 import { AuthGuard } from './guards/auth.guard';
+
+
 const routes: Routes = [
   {
-    path: "",
-    redirectTo: "/home",
-    pathMatch: "full",
+    path: '', 
+    redirectTo: '/home', 
+    pathMatch:'full'
   },
   {
-    path: "home",
-    component: HomeComponent,
+    path: 'home', 
+    component: HomeComponent
   },
   {
-    path: "login",
-    component: LoginComponent,
+    path: 'login', 
+    component: LoginComponent
   },
   {
-    path: "register",
-    component: RegisterComponent,
+    path: 'register', 
+    component: RegisterComponent
   },
   {
-    path: "product",
-    component: ProductComponent,
+    path: 'product', 
+    canActivate: [AuthGuard],
+    component: ProductComponent
   },
   {
-    path: "cart",
-    canActivate:["AuthGuard"],
-    component: CartComponent,
+    path: 'cart', 
+    canActivate: [AuthGuard],
+    component: CartComponent
   },
   {
-    path: "checkout",
-    canActivate:["AuthGuard"],
-    component: CheckoutComponent,
+    path: 'checkout', 
+    canActivate: [AuthGuard],
+    component: CheckoutComponent
+
   },
   {
-    path: "thankyou",
-    canActivate:["AuthGuard"],
-    component: ThankyouComponent,
+    
+    path: 'thankyou', 
+    canActivate: [AuthGuard],
+    component: ThankyouComponent
+
   },
   {
-    path: "footer",
-    canActivate:["AuthGuard"],
-    component: FooterComponent,
-  },
+    path: 'footer', 
+    canActivate: [AuthGuard],
+    component: FooterComponent
+  }
+
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
